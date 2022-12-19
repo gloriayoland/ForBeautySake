63f03060a71ef0278a0d403b08f620f44d4b6a5f
Gloria Yoland <gloriays289@gmail.com>
2022-12-19 16:04:42 +0700

    admin feed

--- app/src/main/java/com/example/forbeautysake/nav_fragment/AdminFeedFragment.java
+++ app/src/main/java/com/example/forbeautysake/nav_fragment/AdminFeedFragment.java
@@ -2,65 +2,83 @@ package com.example.forbeautysake.nav_fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.forbeautysake.Adapter.SectionPagerAdapter;
 import com.example.forbeautysake.R;
+import com.example.forbeautysake.feed_fragment.MyReviewFragment;
+import com.example.forbeautysake.feed_fragment.PublicReviewFragment;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdminFeedFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AdminFeedFragment extends Fragment {
+    // define variables
+    TabLayout tabLayout;
+    TabItem publicReview;
+    ViewPager vp;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    View myFragment;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public AdminFeedFragment (){
 
-    public AdminFeedFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminFeedFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdminFeedFragment newInstance(String param1, String param2) {
-        AdminFeedFragment fragment = new AdminFeedFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public static FeedFragment getInstance(){
+        return new FeedFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_feed, container, false);
+        myFragment = inflater.inflate(R.layout.fragment_admin_feed, container, false);
+
+        // find components by id according to the defined variable
+        vp = myFragment.findViewById(R.id.viewPager);
+        tabLayout = myFragment.findViewById(R.id.tabLayout);
+        publicReview = myFragment.findViewById(R.id.publicReview);
+
+
+        return myFragment;
     }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //set view pager
+        setUpViewPager(vp);
+        tabLayout.setupWithViewPager(vp);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                vp.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private void setUpViewPager(ViewPager viewPager) {
+        //set adapter for view pager
+        SectionPagerAdapter adapter = new SectionPagerAdapter(getChildFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+    }
+
 }
